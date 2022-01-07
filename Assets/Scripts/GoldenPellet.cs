0cbd9ae3053dd67ec5be031f98d0670c7febb030
finalfantasy6464 <finalfantasy6464@sky.com>
2022-01-07 22:18:38 +0000

    07/01/2022 -- Commit
    Changes to some particle systems to make them feel more alive. Changes to every exit, making them prefabs with lights behind them.
    
    Changes to UI Golden pellet indicator to show where it is after collecting all pellets.

--- Assets/Scripts/GoldenPellet.cs
+++ Assets/Scripts/GoldenPellet.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GoldenPellet : MonoBehaviour
 {
-
 	PlayerController playercontroller;
 	GameObject Player;
 
---
+++ Assets/Scripts/ParticleEmitRandom.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmitRandom : MonoBehaviour, IPausable
+{
+	public Vector2 center;
+	public Vector2 size;
+
+	private float trigger;
+	public float firerate;
+
+    public float fireratemin;
+    public float fireratemax;
+
+	public GameObject Particle;
+
+    public bool isPaused { get; set; }
+
+    void Update(){
+
+        if (!isPaused)
+            UnPausedUpdate();
+	}
+
+	public void SpawnParticle()
+    {
+		Vector2 pos = center + new Vector2 (Random.Range(-size.x / 2, size.x / 2),Random.Range(-size.y / 2, size.y / 2));
+		GameObject newParticle = Instantiate (Particle, pos, Quaternion.identity);
+        PauseControl.TryAddPausable(newParticle);
+    }
+
+	void OnDrawGizmosSelected()
+    {
+		Gizmos.color = new Color (0.5f, 0.0f, 0.5f, 0.25f);
+		Gizmos.DrawCube (center, size);
+	}
+
+    public void OnPause()
+    { }
+
+    public void OnUnpause()
+    { }
+
+    public void UnPausedUpdate()
+    {
+        trigger += Time.deltaTime;
+
+        if (trigger >= firerate)
+        {
+            trigger = 0.0f;
+            SpawnParticle();
+            SetNewFireRate();
+        }
+    }
+
+    private void SetNewFireRate()
+    {
+        firerate = Random.Range(fireratemin, fireratemax);
+    }
+
+    public void OnDestroy()
+    {
+        PauseControl.TryRemovePausable(gameObject);
+    }
+}
