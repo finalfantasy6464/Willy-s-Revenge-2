357085e9dbc70a7bfa5f8a56206a6163dd372012
finalfantasy6464 <finalfantasy6464@sky.com>
2022-03-18 11:27:10 +0000

    18/03/22 -- Commit
    Changes to Big Orange animation.

--- Assets/BigOrangeEntrance.cs
+++ Assets/BigOrangeEntrance.cs
@@ -10,12 +10,13 @@ public class BigOrangeEntrance : MonoBehaviour
 
     public void SpawnOrange()
     {
+        BO.transform.position = new Vector3(-1.86f, -5.48f, -20f);
         BO.m_animator.enabled = true;
         BO.m_animator.SetFloat("EntranceSpeed", 1);
     }
-
     public void destroySelf()
-    { 
+    {
         Destroy(gameObject);
     }
+
 }
